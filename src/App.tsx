e5550cad33ef4b7377fdb0fd07ff7433a7d4ef5c
Daniel Lins <dlins300@gmail.com>
2022-03-04 17:53:25 -0300

    Revert "Aplicação concluída faltando apenas separar o código em componentes para uma melhor organização"
    This reverts commit 8787abe0149e8ea923783af35b34c3992b7c0243.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { Home } from "./Pages/Home/Home";
-
-import GlobalStyle from "./styles/global";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <GlobalStyle />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
